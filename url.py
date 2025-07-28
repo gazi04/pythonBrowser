ef4afaa9e07b2c0da213c8a43b3e81c32bb577af
gazi04 <gazmendhalili2016@gmail.com>
2025-07-28 18:01:15 +0200

    :sparkles: Added support for https, file and data schemas
    This commit adds TLS to encrypt, decrypt http request so that it
    supports https. Updated the headers by adding a User-Agent, a simple
    file schema is implemented to give the browser the feature to open local
    files and also the data schema for inline HTML content inside the url.

--- url.py
+++ url.py
@@ -1,56 +1,104 @@
 import socket
+import ssl
+import os
 
 class URL:
-    def __init__(self, url: str) -> None:
-        self.schema, url = url.split('://', 1)
-        assert self.schema == "http"
+    def __init__(self, url='http://localhost:8080/browser/index.html') -> None:
+        self.__prepareUrl(url)
 
+    def __prepareUrl(self, url) -> None:
+        self.scheme, url = url.split(':', 1)
+        assert self.scheme in ['http', 'https', 'file', 'data']
+        if self.scheme != 'data': url = url[2:]
+        if self.scheme == 'file':
+            self.__handleFileSchema(url)
+            return
+
+        if self.scheme == 'data':
+            self.__handleDataSchema(url)
+            return
+
+        self.__handleHttpSchema(url)
+
+    def __prepareHeaders(self, request: str) -> str:
+        request += 'Host: {}\r\n'.format(self.host)
+        request += 'User-Agent: PyBrowser\r\n'
+        request += '\r\n'
+
+        return request
+
+    def __handleFileSchema(self, path: str):
+        if os.path.exists(path) == False:
+            print(f"File not found.\nPyBrowser can't find the file at {path}")
+            return
+        
+        directory = os.listdir(path)
+        for item in directory:
+            # DOESNT DIPSLAY HIDDEN FOLDERS
+            if item[0] == '.': continue
+            print(item)
+
+    def __handleHttpSchema(self, url: str) -> None:
         self.host, url = url.split('/', 1)
+        if ':' in self.host:
+            self.host, port = self.host.split(':', 1)
+            self.port = int(port)
+        else:
+            self.port = 443 if self.scheme == 'https' else 80
         self.path = '/' + url
 
-    def request(self):
-        s = socket.socket(
+        self.socket = socket.socket(
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
             proto=socket.IPPROTO_TCP
         )
 
-        s.connect((self.host, 8080))
+        if self.scheme == 'https':
+            context = ssl.create_default_context()
+            self.socket = context.wrap_socket(self.socket, server_hostname=self.host)
 
-        request = "GET {} HTTP/1.0\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "\r\n"
-        s.send(request.encode("utf8"))
+    def __handleDataSchema(self, url: str) -> None:
+        print(url)
 
-        response = s.makefile(mode='r', encoding='utf8', newline='\r\n')
+    def request(self) -> str:
+        self.socket.connect((self.host, self.port))
+
+        request = 'GET {} HTTP/1.0\r\n'.format(self.path)
+        request = self.__prepareHeaders(request)
+
+        self.socket.send(request.encode('utf8'))
+
+        response = self.socket.makefile(mode='r', encoding='utf8', newline='\r\n')
         statusline = response.readline()
-        version, status, explenation = statusline.split(" ", 2)
+        version, status, explenation = statusline.split(' ', 2)
 
         response_headers = {}
         while True:
             line = response.readline()
-            if line == "\r\n": break
-            header, value = line.split(":", 1)
+            if line == '\r\n': break
+            header, value = line.split(':', 1)
             response_headers[header.casefold()] = value.strip()
 
         assert 'transfer-encoding' not in response_headers
         assert 'content-encoding' not in response_headers
 
         content = response.read()
-        s.close()
+        self.socket.close()
         return content
 
-    def show(self, body):
+    def show(self, body) -> None:
+        cleaned_body = ''
         in_tag = False
         for char in body:
-            if char == "<":
+            if char == '<':
                 in_tag = True
-            elif char == ">":
+            elif char == '>':
                 in_tag = False
             elif not in_tag:
-                print(char, "")
+                cleaned_body += char
+        print(cleaned_body.strip())
 
-    def load(self):
+    def load(self) -> None:
         body = self.request()
         self.show(body)
 
