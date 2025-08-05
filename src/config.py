4f6ecb2716e9229280db7242c598b3a3db827824
gazi04 <gazmendhalili2016@gmail.com>
2025-08-05 16:44:31 +0200

    :memo: Updated documentation

--- src/config.py
+++ src/config.py
@@ -1,14 +1,11 @@
-
+# Used as the name of the tkinter window.
 TITLE = "PyBrowser"
 
-# The horizontal spacing (in pixels) for the browser content.
 # Used for the left and right margins.
 HORIZONTAL_STEP = 13
 
-# The vertical spacing (in pixels) for the browser content.
 # Used for the line height and vertical margins.
 VERTICAL_STEP = 18
 
-# The amount of pixels to scroll with each scroll event.
 # Used for mouse wheel and keyboard scrolling.
 SCROLL_STEP = 100
