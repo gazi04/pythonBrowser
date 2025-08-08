7434bba92af3405cf5a0206ad1c605583fe773ca
gazi04 <gazmendhalili2016@gmail.com>
2025-08-08 17:54:13 +0200

    :recycle: Refactored the if-else statement in the prepareLayout() function
    This commits refactors the if-else statements that handles the font
    based on the tags by creating a dictionary where the key is a tag and
    the value is a lambda function for handling tag fonts.

--- src/render/layout.py
+++ src/render/layout.py
@@ -26,77 +26,41 @@ class Layout:
 
         self.line_buffer: list[int, str, Font] = []
 
+        self.tag_handlers = {
+            "br": lambda: self.__breakLine,
+            "/br": lambda: self.__breakLine,
+            "p": lambda: self.__paragraphBreak,
+            "/p": lambda: self.__paragraphBreak,
+            "i": lambda: self.__setFontStyle("italic"),
+            "/i": lambda: self.__setFontStyle("roman"),
+            "b": lambda: self.__setFontWeight("bold"),
+            "/b": lambda: self.__setFontWeight("normal"),
+            "big": lambda: self.__setFontSize(4),
+            "/big": lambda: self.__setFontSize(-4),
+            "small": lambda: self.__setFontSize(-2),
+            "/small": lambda: self.__setFontSize(2),
+            "h1": lambda: self.__setHeading(24),
+            "/h1": lambda: self.__resetHeading(24),
+            "h2": lambda: self.__setHeading(20),
+            "/h2": lambda: self.__resetHeading(20),
+            "h3": lambda: self.__setHeading(18),
+            "/h3": lambda: self.__resetHeading(18),
+            "h4": lambda: self.__setHeading(16),
+            "/h4": lambda: self.__resetHeading(16),
+            "h5": lambda: self.__setHeading(14),
+            "/h5": lambda: self.__resetHeading(14),
+            "h6": lambda: self.__setHeading(12),
+            "/h6": lambda: self.__resetHeading(12),
+        }
+
         self.__prepareLayout()
 
     def __prepareLayout(self) -> None:
         for token in self.tokens:
             if isinstance(token, Text):
                 self.__prepareLine(token)
-            elif token.tag == "br" or token.tag == "/br":
-                self.__flush()
-                self.cursor_y += VERTICAL_STEP
-                self.cursor_x = HORIZONTAL_STEP
-            elif token.tag == "p" or token.tag == "/p":
-                self.__flush()
-                self.cursor_y += VERTICAL_STEP
-            elif token.tag == "i":
-                self.font_style = "italic"
-            elif token.tag == "/i":
-                self.font_style = "roman"
-            elif token.tag == "b":
-                self.font_weight = "bold"
-            elif token.tag == "/b":
-                self.font_weight = "normal"
-            elif token.tag == "small":
-                self.font_size -= 2
-            elif token.tag == "/small":
-                self.font_size += 2
-            elif token.tag == "big":
-                self.font_size += 4
-            elif token.tag == "/big":
-                self.font_size -= 4
-            elif token.tag == "h1":
-                self.__flush()
-                self.font_size += 24
-                self.font_weight = "bold"
-            elif token.tag == "/h1":
-                self.font_size -= 24
-                self.font_weight = "normal"
-            elif token.tag == "h2":
-                self.__flush()
-                self.font_size += 20
-                self.font_weight = "bold"
-            elif token.tag == "/h2":
-                self.font_size -= 20
-                self.font_weight = "normal"
-            elif token.tag == "h3":
-                self.__flush()
-                self.font_size += 18
-                self.font_weight = "bold"
-            elif token.tag == "/h3":
-                self.font_size -= 18
-                self.font_weight = "normal"
-            elif token.tag == "h4":
-                self.__flush()
-                self.font_size += 16
-                self.font_weight = "bold"
-            elif token.tag == "/h4":
-                self.font_size -= 16
-                self.font_weight = "normal"
-            elif token.tag == "h5":
-                self.__flush()
-                self.font_size += 14
-                self.font_weight = "bold"
-            elif token.tag == "/h5":
-                self.font_size -= 14
-                self.font_weight = "normal"
-            elif token.tag == "h6":
-                self.__flush()
-                self.font_size += 12
-                self.font_weight = "bold"
-            elif token.tag == "/h6":
-                self.font_size -= 12
-                self.font_weight = "normal"
+            elif token.tag in self.tag_handlers:
+                self.tag_handlers[token.tag]()
 
         self.height = self.cursor_y + self.line_height
 
@@ -140,3 +104,30 @@ class Layout:
         self.cursor_y = baseline + 1.25 * max_descent
         self.cursor_x = HORIZONTAL_STEP
         self.line_buffer = []
+
+    def __setFontSize(self, size: int) -> None:
+        self.font_size += size
+
+    def __setFontWeight(self, weight: str) -> None:
+        self.font_weight = weight
+
+    def __setFontStyle(self, style: str) -> None:
+        self.font_style = style
+
+    def __breakLine(self) -> None:
+        self.__flush()
+        self.cursor_y += VERTICAL_STEP
+        self.cursor_x = HORIZONTAL_STEP
+
+    def __paragraphBreak(self) -> None:
+        self.__flush()
+        self.cursor_y += VERTICAL_STEP
+
+    def __setHeading(self, size: int) -> None:
+        self.__flush()
+        self.__setFontSize(size)
+        self.__setFontWeight("bold")
+
+    def __resetHeading(self, size: int):
+        self.__setFontSize(-size)
+        self.__setFontWeight("normal")
